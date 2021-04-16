96bafcd5cdb27f74238e1138424d401e389394db
Krzyszof Lityński <klitynski@gog.com>
2021-04-16 11:34:43 +0200

    basic markdown working

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <textarea v-model="text" class="editor">aaa</textarea>
+    <VueMarkdown class="markdown" :source="text"></VueMarkdown>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import VueMarkdown from 'vue-markdown-render';
 
 @Component({
   components: {
     HelloWorld,
-  },
+    VueMarkdown,
+  }
+
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public text = "";
+}
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display:flex;
+  align-content:center;
+  justify-content:space-around;
+
+}
+.markdown,
+.editor {
+  width:48%;
+  min-height: 500px;
+  border:1px solid black;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueMarkdown from 'vue-markdown-render';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  components: {
+    VueMarkdown
+  }
 }).$mount("#app");
